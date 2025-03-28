e62eb47836da934b547fa2f2ebc5ae42743b9fbb
Sahilthakurr24 <tsahil6231@gmail.com>
2025-03-28 22:25:42 +0530

    change the home

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,17 @@
+'use client';
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+
 
 export default function Home() {
+  const router = useRouter();
   return (
-   <div>will continue this shit sonn...
-    <Button type="button" variant={"default"}>Stay Tune</Button>
-   </div>
+  <div className="min-h-screen flex justify-center items-center bg-gray-700 text-white">
+     <div className="text-center">Will continue this shit sonn...
+  <Button onClick={()=>{ router.push("lol.com")}} className = "mt-2 w-full p-0.5 text-white cursor-pointer" type="button" variant={"outline"}>Stay Tune</Button>
+ </div>
+ </div>
   );
 }
