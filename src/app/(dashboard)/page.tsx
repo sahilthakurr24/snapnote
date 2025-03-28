f93428381cc1ee21473b8c495dad0b6a01c86129
Sahilthakurr24 <tsahil6231@gmail.com>
2025-03-28 22:32:25 +0530

    fix issue in home

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -8,10 +8,11 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-  <div className="min-h-screen flex justify-center items-center bg-gray-700 text-white">
-     <div className="text-center">Will continue this shit sonn...
-  <Button onClick={()=>{ router.push("lol.com")}} className = "mt-2 w-full p-0.5 text-white cursor-pointer" type="button" variant={"outline"}>Stay Tune</Button>
- </div>
- </div>
+    <div className="min-h-screen flex justify-center items-center bg-gray-700 text-white">
+      <div className="text-center">
+        Will continue this shit sonn...
+        <Button onClick={() => { router.push("lol.com") }} className="mt-2 w-full p-0.5 text-white cursor-pointer" type="button" variant={"outline"}>Stay Tune</Button>
+      </div>
+    </div>
   );
 }
