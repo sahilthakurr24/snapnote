612720c43bf5fce31cfec3fa44565773e21dfd79
Sahilthakurr24 <tsahil6231@gmail.com>
2025-04-01 18:49:29 +0530

    fix prisma

--- src/lib/primsa.ts
+++ src/lib/primsa.ts
@@ -1,16 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const primaClientSingleton = ()=>{
-    return new PrismaClient();
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-const globalForPrisma =  globalThis as unknown as {prisma : PrismaClient | undefined}
-
- export const prisma = globalForPrisma.prisma ?? primaClientSingleton();
-
-
-
-if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-
-
